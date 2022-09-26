1fbacc3b730be994510bb244208e708422b21629
UrupaDev <taniaivanko@ukr.net>
2022-09-26 09:48:41 +0100

    portfolio

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -5,3 +5,43 @@ const wow = new WOW({
   mobile: true, 
 })
 wow.init(); 
+
+
+
+// window.onscroll = function() {myFunction()};
+  
+// var menu = document.getElementById("my-header");
+// var sticky = menu.offsetTop;
+      
+//   function myFunction() {
+//     if (window.pageYOffset > sticky) {
+//       menu.classList.add("sticky");
+//     } else {
+//       menu.classList.remove("sticky");
+//     }
+  // }
+
+
+// burger menu
+let burger = document.getElementsByClassName('header__burger-menu')[0];
+let header__items = document.getElementsByClassName('header__wrapper')[0];
+let body = document.getElementsByClassName('body')[0];
+// let menu = document.querySelectorAll('.header__menu-item')[0];
+
+burger.addEventListener("click", function(e) {
+    burger.classList.toggle('header__burger-menu_active');
+    header__items.classList.toggle('header__items_active');
+}, false);
+
+
+
+// var burger_button = document.getElementById("header__burger-menu");
+// var sticky_burger = burger_button.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset > sticky_burger) {
+//     burger_button.classList.add("sticky-button");
+//   } else {
+//     burger_button.classList.remove("sticky-button");
+//   }
+// }
