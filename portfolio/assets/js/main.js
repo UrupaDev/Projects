38fc304a59d87227fddc81a5d157fe93012e089d
UrupaDev <taniaivanko@ukr.net>
2022-09-28 11:49:02 +0100

    portfolio

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -26,45 +26,61 @@ wow.init();
 // burger menu
 let burger = document.getElementsByClassName('header__burger-menu')[0];
 let header__items = document.getElementsByClassName('header__wrapper')[0];
-let menu1 = document.querySelectorAll('.nav__item')[0];
-let menu2 = document.querySelectorAll('.nav__item')[1];
-let menu3 = document.querySelectorAll('.nav__item')[2];
-let menu4 = document.querySelectorAll('.nav__item')[3];
+// let menu1 = document.querySelectorAll('.nav__item')[0];
+// let menu2 = document.querySelectorAll('.nav__item')[1];
+// let menu3 = document.querySelectorAll('.nav__item')[2];
+// let menu4 = document.querySelectorAll('.nav__item')[3];
 
 burger.addEventListener("click", function(e) {
     burger.classList.toggle('header__burger-menu_active');
     header__items.classList.toggle('header__items_active');
 }, false);
 
+// close mobile menu on click items
+let menu_items = document.querySelectorAll('.nav__item');
+console.log(menu_items);
 
-// працює але не на клік
-// document.querySelectorAll('li').forEach(function (elem) {
+menu_items.forEach(function(menu_item){
+	console.log(menu_item);
+  
+	menu_item.addEventListener('click', function() {
+		menu_items.forEach(function(menu_item) {
+			menu_item.classList.remove('header__burger-menu_active')
+      burger.classList.toggle('header__burger-menu_active');
+      header__items.classList.toggle('header__items_active');
+		});
+			menu_item.classList.add('header__burger-menu_active');
+	})
+})
+
+//працює але не на клік
+// document.querySelectorAll('.nav__item').forEach(function (elem) {
 // 	elem.classList.add('header__burger-menu_active');
 // });
 
 
-menu1.addEventListener("click", function(e) {
-  burger.classList.toggle('header__burger-menu_active');
-  header__items.classList.toggle('header__items_active');
-  menu1.classList.toggle('header__burger-menu_active');
-}, false);
+// menu1.addEventListener("click", function(e) {
+//   burger.classList.toggle('header__burger-menu_active');
+//   header__items.classList.toggle('header__items_active');
+//   menu1.classList.toggle('header__burger-menu_active');
+// }, false);
 
-menu2.addEventListener("click", function(e) {
-  burger.classList.toggle('header__burger-menu_active');
-  header__items.classList.toggle('header__items_active');
-  menu2.classList.toggle('header__burger-menu_active');
-}, false);
+// menu2.addEventListener("click", function(e) {
+//   burger.classList.toggle('header__burger-menu_active');
+//   header__items.classList.toggle('header__items_active');
+//   menu2.classList.toggle('header__burger-menu_active');
+// }, false);
 
-menu3.addEventListener("click", function(e) {
-  burger.classList.toggle('header__burger-menu_active');
-  header__items.classList.toggle('header__items_active');
-  menu3.classList.toggle('header__burger-menu_active');
-}, false);
+// menu3.addEventListener("click", function(e) {
+//   burger.classList.toggle('header__burger-menu_active');
+//   header__items.classList.toggle('header__items_active');
+//   menu3.classList.toggle('header__burger-menu_active');
+// }, false);
 
-menu4.addEventListener("click", function(e) {
-  burger.classList.toggle('header__burger-menu_active');
-  header__items.classList.toggle('header__items_active');
-  menu4.classList.toggle('header__burger-menu_active');
-}, false);
+// menu4.addEventListener("click", function(e) {
+//   burger.classList.toggle('header__burger-menu_active');
+//   header__items.classList.toggle('header__items_active');
+//   menu4.classList.toggle('header__burger-menu_active');
+// }, false);
 
 
